22a8f58f0e42e1b829b98f728ba494b65a647500
DESKTOP-G800GM0\uriyu <uriyuri00@gmail.com>
2023-03-01 19:36:51 -0800

    yuri

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-const home = require('./publicCtr');
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
@@ -10,5 +9,7 @@ router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 
-// module.exports = home;
+
+
+
 module.exports = router;
